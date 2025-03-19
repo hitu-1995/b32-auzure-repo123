ad87d69b442e391ccfcb3569ccb59d4a3b836ea5
hitu-1995 <hiteshwankhede11@gmail.com>
2025-03-19 09:55:46 +0530

    first commit

---
+++ Demo.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main()
+{
+    printf("First Line by master \n");
+    printf("Second Line by dev1 \n");
+    printf("Third Line by dev2 \n");
+    printf("Fourth Line by dev1 \n");
+    printf("Fifth Line by dev2 \n");
+    printf("Sixth Line by main \n");
+    
+    
+}
